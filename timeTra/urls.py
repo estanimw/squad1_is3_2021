91904bcc2da9dba5853b3dd355584381a4184bd7
Estanislao <emarinwalker@uca.edu.ar>
2021-06-29 23:26:00 -0300

    Cambio settings slas

--- timeTra/urls.py
+++ timeTra/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="home"),
-    path('task/', views.task_create, name='task_create'),
+    path('task', views.task_create, name='task_create'),
     path('task/all', views.task_list, name='task_list'),
     path('task/<int:id>', views.task, name='task')
 ]
